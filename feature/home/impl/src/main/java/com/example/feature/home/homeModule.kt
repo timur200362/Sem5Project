b8622e5349abcdbbeb064c5380b5a5ed9d26f3c8
timur200362 <kh-g-2016@mail.ru>
2024-02-18 16:56:17 +0300

    Интерфейс, но пока не работает

--- feature/home/impl/src/main/java/com/example/feature/home/homeModule.kt
+++ feature/home/impl/src/main/java/com/example/feature/home/homeModule.kt
@@ -2,7 +2,7 @@ package com.example.feature.home
 
 import com.example.feature.home.impl.data.datasource.remote.ApiFactory
 import com.example.feature.home.impl.data.datasource.remote.ApiService
-import com.example.feature.home.impl.data.datasource.MovieRepository
+import com.example.feature.home.impl.data.datasource.MovieRepositoryImpl
 import com.example.feature.home.impl.presentation.MovieViewModel
 import com.example.feature.home.impl.usecase.MovieUseCase
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -10,7 +10,7 @@ import org.koin.dsl.module
 
 val homeModule = module {
     single<ApiService> { ApiFactory().moviesApi }
-    single<MovieRepository> { MovieRepository(get()) }
+    single<MovieRepositoryImpl> { MovieRepositoryImpl(get()) }
     factory<MovieUseCase> { MovieUseCase(get()) }
     viewModel<MovieViewModel>{ MovieViewModel(get()) }
 }
--- feature/home/impl/src/main/java/com/example/feature/home/impl/usecase/MovieUseCase.kt
+++ feature/home/impl/src/main/java/com/example/feature/home/impl/usecase/MovieUseCase.kt
@@ -1,10 +1,10 @@
 package com.example.feature.home.impl.usecase
 
-import com.example.feature.home.impl.data.datasource.MovieRepository
+import com.example.feature.home.impl.data.datasource.MovieRepositoryImpl
 import com.example.feature.home.impl.data.datasource.memory.Movie
 
-class MovieUseCase(private val movieRepository: MovieRepository) {
+class MovieUseCase(private val movieRepositoryImpl: MovieRepositoryImpl) {
     suspend fun execute():List<Movie>{
-        return movieRepository.getMovies()
+        return movieRepositoryImpl.getMovies()
     }
 }
