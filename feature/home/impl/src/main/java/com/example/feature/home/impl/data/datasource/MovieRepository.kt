4a77ec07c50519ec28d20862b5449e09548736d1
timur200362 <kh-g-2016@mail.ru>
2024-02-18 16:56:34 +0300

    Интерфейс, но пока не работает

--- feature/home/impl/src/main/java/com/example/feature/home/impl/data/datasource/MovieRepository.kt
+++ feature/home/impl/src/main/java/com/example/feature/home/impl/data/datasource/MovieRepository.kt
@@ -1,21 +1,13 @@
 package com.example.feature.home.impl.data.datasource
 
-import android.util.Log
 import com.example.feature.home.impl.data.datasource.memory.Movie
-import com.example.feature.home.impl.data.datasource.remote.ApiService
+import com.example.feature.home.impl.data.datasource.remote.ApiFactory
 
-class MovieRepository(private val apiService: ApiService) {
-    suspend fun getMovies(): List<Movie> {
-        return  apiService.loadMovies().docs.map {
-            Movie(
-                it.description,
-                it.genres,
-                it.id,
-                it.name,
-                it.poster,
-                it.rating,
-                it.releaseYears
-            )
-        }
-    }
-}
+//interface MovieRepository {
+//    suspend fun getMovies(): List<Movie>
+//    companion object {
+//        fun getInstance(): MovieRepository{
+//            return MovieRepositoryImpl(ApiFactory())
+//        }
+//    }
+//}
---
+++ feature/home/impl/src/main/java/com/example/feature/home/impl/data/datasource/MovieRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.feature.home.impl.data.datasource
+
+import android.util.Log
+import com.example.feature.home.impl.data.datasource.memory.Movie
+import com.example.feature.home.impl.data.datasource.remote.ApiFactory
+import com.example.feature.home.impl.data.datasource.remote.ApiService
+
+class MovieRepositoryImpl(private val apiService: ApiService) {
+    suspend fun getMovies(): List<Movie> {
+        return  apiService.loadMovies().docs.map {
+            Movie(
+                it.description,
+                it.genres,
+                it.id,
+                it.name,
+                it.poster,
+                it.rating,
+                it.releaseYears
+            )
+        }
+    }
+}
